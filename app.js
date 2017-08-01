3c5c41f6cfd4ac5122c8dbe5e854e3fbd246b6cc
nassim <bladess@hotmail.fr>
2017-08-01 16:58:59 +0200

    test

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 console.log('have fun !');
+var obj = {
+	firstName : null,
+	lastName  :null ,
+	city : null
+}
+
+
+$("input").change(function(){
+		
+		var inp = $(this).attr("name");
+		if(inp === "first_name"){
+			obj.firstName = $(this).val();
+		}
+		if(inp === "last_name"){
+			obj.lastName = $(this).val();
+		}
+		if(inp === "city"){
+			obj.city = $(this).val();
+		}
+		console.log(obj);
+		$("#username").text(obj.firstName+" "+obj.lastName)
+	
+});
